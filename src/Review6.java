c1e20b79ba8c2fea4f64da28406c63d30d8050f9
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-16 12:07:43 +0900

    Review6完成

---
+++ src/Review6.java
@@ -0,0 +1,10 @@
+public class Review6 {
+    public static void main(String[] args) {
+        System.out.println(105 + 5); //①
+        System.out.println(10 - 2.5); //②
+        System.out.println(100 / 3); //③
+        System.out.println(10 * 3.0); //④
+        System.out.println(50.0 / 5); //⑤
+        //修正なし
+    }
+}
