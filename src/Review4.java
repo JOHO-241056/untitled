e3fd3f0741c8fbed81fdb0c070a136c779cec4f1
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-16 11:57:43 +0900

    Review4完成

---
+++ src/Review4.java
@@ -0,0 +1,14 @@
+public class Review4 {
+    public static void main(String[] args) {
+        int number1 = 5; //①  //1number → number1
+        String str = 88 + "88"; //②
+        final double PI = 3.141592; //③
+//        PI = 3.14; //④
+        float circ = 3.14f; //⑤  //3.14 → 3.14f
+        long a = 1000000000; //⑥  //10000000000 →　1000000000
+        boolean flg = true; //⑦　//"true" → true
+        byte b = 35; //⑧
+        int num = 0x735; //⑨
+        char c = 'a'; //⑩  //"a" →　'a'
+    }
+}
