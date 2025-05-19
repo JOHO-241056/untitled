2babbbef97336f6a755d28c6d09de2cfff815b35
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-19 12:26:59 +0900

    Review17完成

---
+++ src/Review17.java
@@ -0,0 +1,12 @@
+public class Review17 {
+    public static void main(String[] args) {
+        int num = 10; //ここに代入される値は正の整数である。
+        int sum = 0;
+        int i = 2;
+        while (i <= num) {
+            if(i % 2 == 0) sum += i;
+            i++;
+        }
+        System.out.println("結果: " + sum);
+    }
+}
