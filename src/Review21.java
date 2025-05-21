80e108a5ebe3e6728bbb402d5a59003f19103c72
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-21 09:44:39 +0900

    Review21完成

---
+++ src/Review21.java
@@ -0,0 +1,10 @@
+public class Review21 {
+    public static void main(String[] args) {
+        int num = 10; //ここに代入される値は正の整数である。
+        int sum = 0;
+        for (int i = 2; i <= num; i++) {
+            if(i % 2 == 0) sum += i;
+        }
+        System.out.println("結果: " + sum);
+    }
+}
