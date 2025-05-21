ef24afdb3bf0b6e8bf3a4d1b9dc6b1bd8972ec9e
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-21 09:40:33 +0900

    Review19完成

---
+++ src/Review19.java
@@ -0,0 +1,12 @@
+public class Review19 {
+    public static void main(String[] args) {
+        int num = 10; //ここに代入される値は正の整数である。
+        int i = 2;
+        int factorial = 1;
+        while (i <= num) {
+            factorial *= i;
+            i++;
+        }
+        System.out.println("結果: " + factorial);
+    }
+}
