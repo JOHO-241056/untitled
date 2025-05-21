1f3aa56098392f180b15ac7a215d5c94f4f5e855
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-21 09:50:12 +0900

    Review23完成

---
+++ src/Review23.java
@@ -0,0 +1,10 @@
+public class Review23 {
+    public static void main(String[] args) {
+        int num = 10; //ここに代入される値は正の整数である。
+        int factorial = 1;
+        for (int i = num; i > 0; i--) {
+            factorial *= i;
+        }
+        System.out.println("結果: " + factorial);
+    }
+}
