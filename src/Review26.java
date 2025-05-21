01f45c3dc0c6ae809c36771b03d161f35a87ba66
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-21 10:07:46 +0900

    Review26完成

---
+++ src/Review26.java
@@ -0,0 +1,14 @@
+public class Review26 {
+    public static void main(String[] args) {
+        int[] numArray = {1, 1, 2, 2, 2, 3, 4, 4, 5, 5, 5};
+        int target = 4; //探索する整数値が代入される。
+        int count = 0;
+        for (int i = 0; i < numArray.length; i++) {
+            if (numArray[i] == target) {
+                count++;
+            }
+        }
+        System.out.println("探索した値: " + target);
+        System.out.println("一致した数: " + count);
+    }
+}
