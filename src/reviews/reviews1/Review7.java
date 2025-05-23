a4af786876ff818c71535a5f1160bdfc47ee5e9f
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-23 11:23:24 +0900

    Review7途中

--- src/reviews/reviews1/Review7.java
+++ src/reviews/reviews1/Review7.java
@@ -2,6 +2,10 @@ package reviews.reviews1;
 
 public class Review7 {
     public static void main(String[] args) {
-        System.out.println(4 * 5 * 2 + 10 + "3" + 6 + 2);
+        System.out.println(4 * 5 * 2 + 10 + "3" + 6 + 2); //①
+        System.out.println("10" + (4 + 3 - 5 + 8 / 4)); //②  Stringに-演算子は使えない
+        System.out.println(15 - 3 * 2 + "5" + 3 * 7); //③
+        System.out.println("5" + 6 * 4 + 7 + 10 / 2); //④
+//        System.out.println((10 + 5) * 3 - "2" + 4 * -5); //⑤ Stringに-演算子は使えない
     }
 }
