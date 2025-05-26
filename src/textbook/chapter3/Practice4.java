880bea64a3d01fd8bc513d7997969b2331169928
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-26 12:10:22 +0900

    練習3-4完成

---
+++ src/textbook/chapter3/Practice4.java
@@ -0,0 +1,14 @@
+package textbook.chapter3;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        boolean tenki = true;
+        if (tenki) { //tenkiだけでよい
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else { //elseの場合の処理をかっこで括れていない
+            System.out.println("映画を見ます");
+            System.out.println("寝ます");
+        }
+    }
+}
