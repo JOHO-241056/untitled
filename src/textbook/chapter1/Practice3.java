04ac4c0b9230bde7dc85c479a1d1fcff06d4dfa2
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-26 11:39:08 +0900

    練習1-3完成

---
+++ src/textbook/chapter1/Practice3.java
@@ -0,0 +1,11 @@
+package textbook.chapter1;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        boolean b = true; //①
+        char c = '駆'; //②
+        double d = 3.14; //③
+        long l = 314159265853979L; //④
+        String s = "ミナトの攻撃！ 敵に15ポイントのダメージを与えた。"; //⑤
+    }
+}
