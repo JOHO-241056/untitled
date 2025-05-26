96a369c24bfca918abdff6d0ff08cc72bd4b1cd6
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-05-26 12:06:15 +0900

    練習3-3完成

---
+++ src/textbook/chapter3/Practice3.java
@@ -0,0 +1,17 @@
+package textbook.chapter3;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        int isHungry = 1; //0もしくは1
+        String food = "とろ〜り３種のチーズ牛丼"; //任意の文字列
+
+        System.out.println("こんにちは");
+        if(isHungry == 0){
+            System.out.println("お腹がいっぱいです");
+        } else {
+            System.out.println("はらぺこです");
+            System.out.println(food + "をいただきます");
+            System.out.println("ごちそうさまでした");
+        }
+    }
+}
