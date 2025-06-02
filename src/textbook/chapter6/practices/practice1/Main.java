daea3864d9d55c84023de5661fe7a6e0b8df9ba3
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-06-02 12:30:55 +0900

    練習6-1途中

---
+++ src/textbook/chapter6/practices/practice1/Main.java
@@ -0,0 +1,10 @@
+package textbook.chapter6.practices.practice1;
+
+import static textbook.chapter6.practices.practice1.comment.Zenhan.*;
+
+public class Main {
+    public static void main(String[] args) {
+        doWarusa();
+
+    }
+}
---
+++ src/textbook/chapter6/practices/practice1/comment/Kouhan.java
@@ -0,0 +1,14 @@
+package textbook.chapter6.practices.practice1.comment;
+
+import static textbook.chapter6.practices.practice1.comment.Zenhan.*;
+
+public class Kouhan {
+    public static void callDeae() {
+        System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+    }
+    public static void showMondokoro() {
+        System.out.println("飛車さん、角さん。もういいでしょう。");
+        System.out.println("この紋所が目にはいらぬか！");
+        doTogame();
+    }
+}
