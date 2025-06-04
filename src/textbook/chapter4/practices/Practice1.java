d2ab09d42f89f1cbfc2dca6c6469128ca279dc48
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-06-04 09:25:13 +0900

    練習4-1修正

--- src/textbook/chapter4/practices/Practice1.java
+++ src/textbook/chapter4/practices/Practice1.java
@@ -1,8 +1,10 @@
 package textbook.chapter4.practices;
 
 public class Practice1 {
-    int[] points = new int[4];
-    double[] weights = new double[5];
-    boolean[] answers = new boolean[3];
-    String[] names = new String[3];
+    public static void main(String[] args) {
+        int[] points = new int[4];
+        double[] weights = new double[5];
+        boolean[] answers = new boolean[3];
+        String[] names = new String[3];
+    }
 }
