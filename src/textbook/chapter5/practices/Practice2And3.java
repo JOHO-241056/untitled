c542718c1a1239320c08f1a89b205b5ac6770d95
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-06-04 10:31:46 +0900

    練習5-3完成

--- src/textbook/chapter5/practices/Practice2And3.java
+++ src/textbook/chapter5/practices/Practice2And3.java
@@ -1,13 +1,20 @@
 package textbook.chapter5.practices;
 
-public class Practice2 {
+public class Practice2And3 {
     public static void main(String[] args) {
         email("fugafuga", "hoge@example.com", "piyopiyo");
+        email("foo@example.com", "bar");
     }
 
     private static void email(String title, String address, String text) {
         System.out.println(address + "に、以下のメールを送信しました。");
         System.out.println("件名: " + title);
+        System.out.println("本文: " + text);
+    }
+
+    private static void email(String address, String text) {
+        System.out.println(address + "に、以下のメールを送信しました。");
+        System.out.println("件名: " + "無題");
         System.out.println("本文: " + text);
     }
 }
