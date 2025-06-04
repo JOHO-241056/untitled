0f2cab18b926f5d0453359b25ea2ddc5377aee96
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-06-04 09:21:11 +0900

    練習4-1完成

---
+++ src/textbook/chapter4/practices/Practice1.java
@@ -0,0 +1,8 @@
+package textbook.chapter4.practices;
+
+public class Practice1 {
+    int[] points = new int[4];
+    double[] weights = new double[5];
+    boolean[] answers = new boolean[3];
+    String[] names = new String[3];
+}
