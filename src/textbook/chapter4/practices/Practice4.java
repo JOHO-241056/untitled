301849e7ea417bbbdb4fb4067fbcbc398da71592
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-06-04 09:32:07 +0900

    練習4-4完成

---
+++ src/textbook/chapter4/practices/Practice4.java
@@ -0,0 +1,14 @@
+package textbook.chapter4.practices;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        int[] numbers = {3, 4, 9};
+        System.out.println("１桁の数字を入力してください");
+        int input = new java.util.Scanner(System.in).nextInt();
+        for(int number : numbers) {
+            if(number == input) {
+                System.out.println("アタリ！");
+            }
+        }
+    }
+}
